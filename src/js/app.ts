7ec769177ea8ccf3c5ba105a5f4a7116962202e8
Robert Haskell <rjhaskell23@gmail.com>
2017-03-20 22:11:50 -0700

    Initial problem start

---
+++ src/js/app.ts
@@ -0,0 +1,16 @@
+namespace Lesson06 {
+    let module: ng.IModule = angular.module('app', [
+        /*-- Third Party Modules --*/
+        'ngResource',
+        'ui.router',
+
+        /*-- Application Modules --*/
+        'app.filters',
+        'app.services',
+        'app.views'
+    ]);
+
+    module.config(Configuration);
+
+    module.run(Run);
+}
